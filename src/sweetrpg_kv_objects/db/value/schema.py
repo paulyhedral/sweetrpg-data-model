07d068fed2553d26dafd9f22fec46cdc67630468
Paul Schifferer <paul@schifferers.net>
2023-09-24 08:26:58 -0700

    Fix ValueSchema

--- src/sweetrpg_kv_objects/db/value/schema.py
+++ src/sweetrpg_kv_objects/db/value/schema.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
-Schema for Store data.
+Schema for Value data.
 """
 
 from marshmallow import fields
-from sweetrpg_kv_objects.model.store import Store
+from sweetrpg_kv_objects.model.value import Value
 from sweetrpg_model_core.schema.base import BaseSchema
 
 
-class StoreSchema(BaseSchema):
-    model_class = Store
+class ValueSchema(BaseSchema):
+    model_class = Value
 
     name = fields.String(required=True)  # , load_only=True)
     description = fields.String(required=True)  # , load_only=True)
