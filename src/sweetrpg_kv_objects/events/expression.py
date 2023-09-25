193228b6fc0bd3f73f8b37cdad91ae01502d8503
Paul Schifferer <paul@schifferers.net>
2023-09-25 06:34:03 -0700

    Move events to their own module

--- src/sweetrpg_kv_objects/events/expression.py
+++ src/sweetrpg_kv_objects/events/expression.py
@@ -4,18 +4,19 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 
 import logging
-from sweetrpg_model_core.model.base import BaseModel
+from sweetrpg_model_core.model.base import SimpleModel
 
 
-class KeyEvent(BaseModel):
-    """A model object representing an event on store's key."""
+class ExpressionEvent(SimpleModel):
+    """A model object representing a change in a key's expression."""
 
     def __init__(self, *args, **kwargs):
-        """Creates a new Key object."""
+        """Creates a new ExpressionEvent object."""
         logging.debug("args: %s, kwargs: %s", args, kwargs)
 
         super().__init__(*args, **kwargs)
 
         self.store = kwargs.get("store")  # the store where the event occurred
+        self.event = kwargs.get("event")  # the event that occurred
         self.key = kwargs.get("key")  # the key whose value changed
         self.occurred = kwargs.get("occurred")  # the date/time the event occurred
--- src/sweetrpg_kv_objects/events/key.py
+++ src/sweetrpg_kv_objects/events/key.py
@@ -4,18 +4,19 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 
 import logging
-from sweetrpg_model_core.model.base import BaseModel
+from sweetrpg_model_core.model.base import SimpleModel
 
 
-class ValueEvent(BaseModel):
-    """A model object representing an event on a value."""
+class KeyEvent(SimpleModel):
+    """A model object representing an event on store's key."""
 
     def __init__(self, *args, **kwargs):
-        """Creates a new ValueEvent object."""
+        """Creates a new Key object."""
         logging.debug("args: %s, kwargs: %s", args, kwargs)
 
         super().__init__(*args, **kwargs)
 
         self.store = kwargs.get("store")  # the store where the event occurred
+        self.event = kwargs.get("event")  # the event that occurred
         self.key = kwargs.get("key")  # the key whose value changed
         self.occurred = kwargs.get("occurred")  # the date/time the event occurred
