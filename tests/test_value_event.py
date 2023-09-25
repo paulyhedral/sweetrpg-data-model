4081907e8bfeb1e609fb40e35204e52a633fa7ca
Paul Schifferer <paul@schifferers.net>
2023-09-25 08:10:43 -0700

    Parse dates

--- tests/test_value_event.py
+++ tests/test_value_event.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
+Test cases for ValueEvent.
 """
 
 from sweetrpg_kv_objects.events.value import ValueEvent
@@ -12,7 +13,7 @@ event_json = """
     "store": "test-store",
     "event": "test-event",
     "key": "test-key",
-    "occurred": "2021-09-13T07:55:00.001"
+    "occurred_at": "2021-09-13T07:55:00.001"
 }
 """
 event_datetime = datetime(2021, 9, 13, 7, 55, 0, 1000)
@@ -25,4 +26,4 @@ def test_event_from_json():
     assert a.store == "test-store"
     assert a.event == "test-event"
     assert a.key == "test-key"
-    assert a.occurred == event_datetime
+    assert a.occurred_at == event_datetime
