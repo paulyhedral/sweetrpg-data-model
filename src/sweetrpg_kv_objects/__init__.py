9dd1b4d61dd6905f01f26f6ba5818416d32a44d5
Paul Schifferer <paul@schifferers.net>
2024-06-08 07:24:58 -0700

    Metadata

--- src/sweetrpg_kv_objects/__init__.py
+++ src/sweetrpg_kv_objects/__init__.py
@@ -4,12 +4,12 @@ Metadata.
 """
 
 __title__ = "sweetrpg-kv-objects"
-__description__ = "Model objects for SweetRPG Kv"
+__description__ = "Model objects for SweetRPG Key-Value"
 __url__ = "https://sweetrpg.com"
 __version__ = "0.0.16"
 __build__ = 0x000000
 __author__ = "Paul Schifferer"
 __author_email__ = "dm@sweetrpg.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2023 SweetRPG"
+__copyright__ = "Copyright 2024 SweetRPG"
 __cake__ = "\u2728 \U0001f370 \u2728"
