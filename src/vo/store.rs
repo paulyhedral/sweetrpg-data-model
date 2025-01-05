4a6c669c1bc32ed449294aaba29834d36fb03eb8
Paul Schifferer <paul@schifferers.net>
2025-01-04 21:40:31 -0800

    feat: Add unit tests for Store and Value VOs

--- src/vo/store.rs
+++ src/vo/store.rs
@@ -23,4 +23,42 @@ pub struct StoreVO {
 // ----------------------------------------------------------------------
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use sweetrpg_model_core::vo::auditable::AuditableVO;
+
+    #[test]
+    fn serialize_store_vo() {
+        let store1 = StoreVO {
+            id: "12345".to_string(),
+            name: "store".to_string(),
+            description: "description".to_string(),
+            current_snapshot_id: "67890".to_string(),
+            notes: "notes".to_string(),
+            tags: vec![],
+            auditable: AuditableVO {
+                created_at: 123456789,
+                created_by: "user".to_string(),
+                updated_at: 123456789,
+                updated_by: "user".to_string(),
+                deleted_at: None,
+                deleted_by: None,
+            },
+        };
+
+        let json = serde_json::to_string(&store1).unwrap();
+        let store2: StoreVO = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(store1.id, store2.id);
+        assert_eq!(store1.name, store2.name);
+        assert_eq!(store1.description, store2.description);
+        assert_eq!(store1.current_snapshot_id, store2.current_snapshot_id);
+        assert_eq!(store1.notes, store2.notes);
+        assert_eq!(store1.auditable.created_at, store2.auditable.created_at);
+        assert_eq!(store1.auditable.created_by, store2.auditable.created_by);
+        assert_eq!(store1.auditable.updated_at, store2.auditable.updated_at);
+        assert_eq!(store1.auditable.updated_by, store2.auditable.updated_by);
+        assert_eq!(store1.auditable.deleted_at, store2.auditable.deleted_at);
+        assert_eq!(store1.auditable.deleted_by, store2.auditable.deleted_by);
+    }
+}
--- src/vo/value.rs
+++ src/vo/value.rs
@@ -24,4 +24,44 @@ pub struct ValueVO {
 // ----------------------------------------------------------------------
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use sweetrpg_model_core::vo::auditable::AuditableVO;
+
+    #[test]
+    fn serialize_value_vo() {
+        let value1 = ValueVO {
+            id: "12345".to_string(),
+            store_id: "67890".to_string(),
+            key_id: "abcde".to_string(),
+            snapshot_id: "fghij".to_string(),
+            value: "value".to_string(),
+            notes: "notes".to_string(),
+            tags: vec![],
+            auditable: AuditableVO {
+                created_at: 123456789,
+                created_by: "user".to_string(),
+                updated_at: 123456789,
+                updated_by: "user".to_string(),
+                deleted_at: None,
+                deleted_by: None,
+            },
+        };
+
+        let json = serde_json::to_string(&value1).unwrap();
+        let value2: ValueVO = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(value1.id, value2.id);
+        assert_eq!(value1.store_id, value2.store_id);
+        assert_eq!(value1.key_id, value2.key_id);
+        assert_eq!(value1.snapshot_id, value2.snapshot_id);
+        assert_eq!(value1.value, value2.value);
+        assert_eq!(value1.notes, value2.notes);
+        assert_eq!(value1.auditable.created_at, value2.auditable.created_at);
+        assert_eq!(value1.auditable.created_by, value2.auditable.created_by);
+        assert_eq!(value1.auditable.updated_at, value2.auditable.updated_at);
+        assert_eq!(value1.auditable.updated_by, value2.auditable.updated_by);
+        assert_eq!(value1.auditable.deleted_at, value2.auditable.deleted_at);
+        assert_eq!(value1.auditable.deleted_by, value2.auditable.deleted_by);
+    }
+}
