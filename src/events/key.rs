e8a90a9e630c0158074ceb4545ce72a715efb7d5
Paul Schifferer <paul@schifferers.net>
2025-01-04 20:39:55 -0800

    Add unit tests

--- src/events/key.rs
+++ src/events/key.rs
@@ -10,3 +10,32 @@ pub struct KeyEvent {
     pub key: String,
     pub occurred_at: u64,
 }
+
+// ----------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::constants::EVENT_EXPRESSION_CHANGED;
+    use serde_json;
+
+    #[test]
+    fn serialize_key_event() {
+        let event1 = KeyEvent {
+            id: "12345".to_string(),
+            store_id: "67890".to_string(),
+            event: EVENT_EXPRESSION_CHANGED.to_string(),
+            key: "key".to_string(),
+            occurred_at: 12345,
+        };
+
+        let json = serde_json::to_string(&event1).unwrap();
+        let event2: KeyEvent = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(event1.id, event2.id);
+        assert_eq!(event1.store_id, event2.store_id);
+        assert_eq!(event1.event, event2.event);
+        assert_eq!(event1.key, event2.key);
+        assert_eq!(event1.occurred_at, event2.occurred_at);
+    }
+}
--- src/events/value.rs
+++ src/events/value.rs
@@ -1,4 +1,9 @@
+/**
+ * Value event object.
+ * @paulyhedral
+ */
 use serde::{Deserialize, Serialize};
+
 
 /// Value event.
 /// This object represents a change in a key's value.
@@ -9,4 +14,33 @@ pub struct ValueEvent {
     pub event: String,
     pub key: String,
     pub occurred_at: u64,
+}
+
+// ----------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::constants::EVENT_RECALCULATE;
+    use serde_json;
+
+    #[test]
+    fn serialize_value_event() {
+        let event1 = ValueEvent {
+            id: "12345".to_string(),
+            store_id: "67890".to_string(),
+            event: EVENT_RECALCULATE.to_string(),
+            key: "key".to_string(),
+            occurred_at: 123456789,
+        };
+
+        let json = serde_json::to_string(&event1).unwrap();
+        let event2: ValueEvent = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(event1.id, event2.id);
+        assert_eq!(event1.store_id, event2.store_id);
+        assert_eq!(event1.event, event2.event);
+        assert_eq!(event1.key, event2.key);
+        assert_eq!(event1.occurred_at, event2.occurred_at);
+    }
 }
