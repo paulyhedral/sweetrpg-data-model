6daeb7e2c8f1ffdfc0769b0d03b3a84d3fda39b5
Paul Schifferer <paul@schifferers.net>
2025-01-19 06:13:00 -0800

    feat: Add back generic response

---
+++ src/events/response.rs
@@ -0,0 +1,34 @@
+/**
+ * Generic response
+ * @paulyhedral
+ */
+use serde::{Deserialize, Serialize};
+
+
+/// Generic response.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Response {
+    pub status: String,
+    pub message: String,
+}
+
+// ----------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use serde_json;
+
+    #[test]
+    fn serialize_response() {
+        let response1 = super::Response {
+            status: RESPONSE_PONG.to_string(),
+            message: "pong".to_string(),
+        };
+
+        let json = serde_json::to_string(&response1).unwrap();
+        let response2: super::Response = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(response1.status, response2.status);
+        assert_eq!(response1.message, response2.message);
+    }
+}
