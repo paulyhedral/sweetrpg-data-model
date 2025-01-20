675356d60aeaafa0144471662f1b3451cda03193
Paul Schifferer <paul@schifferers.net>
2025-01-20 06:25:58 -0800

    fix: Change static to const

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -4,23 +4,23 @@
  */
 
 // Event types
-pub static EVENT_EXPRESSION_CHANGED: &str = "expression_changed";
-pub static EVENT_RECALCULATE: &str = "recalculate";
-pub static EVENT_VALUE_CHANGED: &str = "value_changed";
-pub static EVENT_PING: &str = "ping";
+pub const EVENT_EXPRESSION_CHANGED: &str = "expression_changed";
+pub const EVENT_RECALCULATE: &str = "recalculate";
+pub const EVENT_VALUE_CHANGED: &str = "value_changed";
+pub const EVENT_PING: &str = "ping";
 
 // Response values
-pub static RESPONSE_SUCCESS: &str = "success";
-pub static RESPONSE_PONG: &str = "pong";
-pub static RESPONSE_IGNORED: &str = "ignored";
-pub static RESPONSE_ERROR: &str = "error";
+pub const RESPONSE_SUCCESS: &str = "success";
+pub const RESPONSE_PONG: &str = "pong";
+pub const RESPONSE_IGNORED: &str = "ignored";
+pub const RESPONSE_ERROR: &str = "error";
 
 // Key types
-pub static KEY_TYPE_INTEGER: &str = "number";
-pub static KEY_TYPE_STRING: &str = "string";
-pub static KEY_TYPE_BOOLEAN: &str = "boolean";
-pub static KEY_TYPE_DATA: &str = "data";
+pub const KEY_TYPE_INTEGER: &str = "number";
+pub const KEY_TYPE_STRING: &str = "string";
+pub const KEY_TYPE_BOOLEAN: &str = "boolean";
+pub const KEY_TYPE_DATA: &str = "data";
 
 // Key encodings
-pub static KEY_ENCODING_PLAIN: &str = "plain";
-pub static KEY_ENCODING_BASE64: &str = "base64";
+pub const KEY_ENCODING_PLAIN: &str = "plain";
+pub const KEY_ENCODING_BASE64: &str = "base64";
