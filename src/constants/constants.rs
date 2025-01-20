fe501435db6d369967bbf752dfcf0a22174128ba
Paul Schifferer <paul@schifferers.net>
2025-01-19 19:36:15 -0800

    feat: Add response constant for ignored

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -9,9 +9,10 @@ pub static EVENT_RECALCULATE: &str = "recalculate";
 pub static EVENT_VALUE_CHANGED: &str = "value_changed";
 pub static EVENT_PING: &str = "ping";
 
-// Response values 
+// Response values
 pub static RESPONSE_SUCCESS: &str = "success";
 pub static RESPONSE_PONG: &str = "pong";
+pub static RESPONSE_IGNORED: &str = "ignored";
 pub static RESPONSE_ERROR: &str = "error";
 
 // Key types
